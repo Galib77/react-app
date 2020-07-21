25f36bcd9e7e972549cd27bb685e0a48ec900fe3
Galib77 <razagalib7@gmail.com>
2020-07-21 17:41:18 +0530

    Updates

---
+++ precache-manifest.ae61446569cdc5f937b3706f12a370e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6870fa2ff1b8b07dbb180a31af44ba3",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9394d8987f1770d4088f",
+    "url": "/react-app/static/css/main.f5952a56.chunk.css"
+  },
+  {
+    "revision": "d63d44d66b7da98370d3",
+    "url": "/react-app/static/js/2.967ea7fc.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-app/static/js/2.967ea7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9394d8987f1770d4088f",
+    "url": "/react-app/static/js/main.e7815997.chunk.js"
+  },
+  {
+    "revision": "1656fcc2cdd317ecf59f",
+    "url": "/react-app/static/js/runtime-main.030c3bf5.js"
+  }
+]);
